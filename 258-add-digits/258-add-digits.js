7636f5a7c31094a5b01561c5c1ff90ff9e9c1896
allotei5 <56276956+allotei5@users.noreply.github.com>
2022-09-22 12:01:20 +0000

    Time: 101 ms (68.54%), Space: 43.8 MB (58.83%) - LeetHub

--- 258-add-digits/258-add-digits.js
+++ 258-add-digits/258-add-digits.js
@@ -3,20 +3,5 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    let sum = 0;
-    let digit = 0;
-    
-    if (num < 10) {
-        return num
-    }
-    
-    while (num !== 0) {
-        digit = num % 10
-    
-        sum += digit
-        
-        num = Math.floor(num/10)
-    }
-    
-    return addDigits(sum)
+    return 1 + (num - 1) % 9;
 };
