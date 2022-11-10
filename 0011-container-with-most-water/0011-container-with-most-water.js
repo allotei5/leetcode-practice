e6657aeebcd7c7a97e4cd575d9120f4b1fa51f85
allotei5 <56276956+allotei5@users.noreply.github.com>
2022-11-10 12:15:22 +0000

    Time: 130 ms (51.95%), Space: 50.1 MB (17.93%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+    let left = 0
+    let right = height.length - 1
+    let maxArea = 0
+    
+    while (left < right) {
+        maxArea = Math.max(maxArea, Math.min(height[left], height[right])*Math.abs(right-left))
+        
+        if(height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return maxArea
+    
+};
